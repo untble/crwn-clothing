2eaafbc6ae6c745230b9656fe2127d6a666d3a2b
Vitalii <dvorskiy.vitaliy@gmail.com>
2021-04-05 21:52:27 +0300

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
-import {setCurrentUser} from "./redux/user/user.action";
+import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-    unsubscribeFromAuth = null;
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
-    componentDidMount() {
-        const {setCurrentUser} = this.props;
-
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth);
-
-                userRef.onSnapshot(snapshot => {
-                    setCurrentUser({
-                        id: snapshot.id,
-                        ...snapshot.data()
-                    });
-                });
-            } else {
-                setCurrentUser(userAuth)
-            }
-        })
-    }
-
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage}/>
-                </Switch>
-            </div>
-        );
-    }
-
-
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
 
-import store from "./redux/store";
 
 import './index.css';
 import App from './App';
@@ -11,11 +9,9 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
-        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
